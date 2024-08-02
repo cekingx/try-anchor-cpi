6f4c63308e9fadff9716a73980bb6ff6cbb18016
Dirga Yasa <cekingx@gmail.com>
2024-08-02 11:33:39 +0800

    feat: cpi works

--- programs/puppet/src/lib.rs
+++ programs/puppet/src/lib.rs
@@ -7,10 +7,31 @@ pub mod puppet {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn set_data(ctx: Context<SetData>, data: u64) -> Result<()> {
+        ctx.accounts.puppet.data = data;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8)]
+    pub puppet: Account<'info, Data>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct SetData<'info> {
+    #[account(mut)]
+    pub puppet: Account<'info, Data>,
+}
+
+#[account]
+pub struct Data {
+    pub data: u64,
+}
--- tests/puppet.ts
+++ tests/puppet.ts
@@ -1,16 +1,39 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Puppet } from "../target/types/puppet";
+import { Keypair } from "@solana/web3.js";
+import { PuppetMaster } from "../target/types/puppet_master";
 
 describe("puppet", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Puppet as Program<Puppet>;
+  const puppetMasterProgram = anchor.workspace.PuppetMaster as Program<PuppetMaster>;
+  const storage = Keypair.generate();
 
-  it("Is initialized!", async () => {
+  it("Does CPI", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const initAccount = {
+      puppet: storage.publicKey,
+    }
+    await program.methods
+    .initialize()
+    .accounts(initAccount)
+    .signers([storage])
+    .rpc();
+
+    const CPIAccount = {
+      puppet: storage.publicKey,
+      puppetProgram: program.programId,
+    };
+    await puppetMasterProgram.methods
+      .pullStrings(new anchor.BN(1))
+      .accounts(CPIAccount)
+      .rpc()
+
+    const result = await program.account.data.fetch(storage.publicKey);
+    console.log(result);
   });
 });
